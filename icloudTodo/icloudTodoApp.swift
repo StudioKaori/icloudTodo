85e39d71d38953ea7b1b8fab3377d11d45625ba1
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2023-05-17 12:07:09 +0200

    Initial Commit

---
+++ icloudTodo/icloudTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  icloudTodoApp.swift
+//  icloudTodo
+//
+//  Created by Kaori Persson on 2023-05-17.
+//
+
+import SwiftUI
+
+@main
+struct icloudTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
